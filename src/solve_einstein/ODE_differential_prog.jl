07cc5afa744ce63080a3b81c29419170e77418e9
Miles Cochran-Branson <mcochranbranson@gmail.com>
2022-11-11 15:45:45 +0100

    some small code updates; sometimes works sometimes not

--- src/solve_einstein/ODE_differential_prog.jl
+++ src/solve_einstein/ODE_differential_prog.jl
@@ -13,16 +13,21 @@ using Random, CUDA, StaticArrays
 using Optimization, OptimizationOptimisers, OptimizationOptimJL
 import ModelingToolkit: Interval, infimum, supremum
 
+using Plots, LaTeXStrings
+
 include("../utils/general_utils.jl")
 
 # define some consts
 const G = 6.67e-11 # m²/kg²
-const M = 1.989e30 #kg
+const M = 1.989e+30 #kg
 const AU = 1.496e11 # meters
 const yr = 3.154e7 #seconds
 # change units
-const GM = G*M*yr^2 / AU^3 # AU^3/yr^2; Kepler III says this is 4π^2
-const c = 3e8 * (yr/AU) # AU / yr
+const GM = Float32(G*M*yr^2 / AU^3) # AU^3/yr^2; Kepler III says this is 4π^2
+const c = Float32(3e8 * (yr/AU)) # AU / yr
+const r_peri = 0.98f0 # AU
+const semi_major = 1.f0
+const v_peri = sqrt(GM * ((2.f0/r_peri) - (1.f0/semi_major)))
 const ricci_r = 2*GM/c^2
 
 @parameters r
@@ -32,16 +37,16 @@ Dr = Differential(r)
 Drr = Differential(r)^2
 
 eqns = [
-    4*Dr(A(r))*((B(r))^2) - 2*r*Drr(B(r))*A(r)*B(r) + 2*Dr(A(r))*Dr(B(r))*B(r) + r*((Dr(B(r)))^2)*A(r) ~ 0,
-    r*Dr(A(r))*B(r) + 2*((A(r))^2)*B(r) - 2*A(r)*B(r) - r*Dr(B(r))*A(r) ~ 0,
-    -2*r*Drr(B(r))*A(r)*B(r) + r*Dr(A(r))*Dr(B(r))*B(r) + r*((Dr(B(r)))^2)*A(r) - 4*Dr(B(r))*A(r)*B(r) ~ 0
+    4.f0*Dr(A(r))*((B(r))^2) - 2.f0*r*Drr(B(r))*A(r)*B(r) + 2.f0*Dr(A(r))*Dr(B(r))*B(r) + r*((Dr(B(r)))^2)*A(r) ~ 0.f0,
+    r*Dr(A(r))*B(r) + 2.f0*((A(r))^2)*B(r) - 2.f0*A(r)*B(r) - r*Dr(B(r))*A(r) ~ 0.f0,
+    -2.f0*r*Drr(B(r))*A(r)*B(r) + r*Dr(A(r))*Dr(B(r))*B(r) + r*((Dr(B(r)))^2)*A(r) - 4.f0*Dr(B(r))*A(r)*B(r) ~ 0.f0
 ]
 
-r_min = 1.0
-r_max = 10.0
+r_min = 0.1f0
+r_max = 2.f0
 bcs = [
-    B(r_max) ~ -1,
-    A(r_max) ~ 1,
+    B(r_max) ~ -1.f0,
+    A(r_max) ~ 1.f0,
 ]
 
 domains = [r ∈ Interval(r_min, r_max)]
@@ -53,21 +58,25 @@ domains = [r ∈ Interval(r_min, r_max)]
 # -------------------------------------------------------------------------------------
 # Solve for Newtonian Gravity!
 function newton_gravity(ddu,du,u,p,t)
-    r = sqrt(u[1]^2 + u[2]^2)
-    ddu[1] = GM*u[1]/r^1.5
-    ddu[2] = GM*u[2]/r^1.5
+    r_sqrd = (u[1]^2 + u[2]^2)
+    ddu[1] = -GM*u[1]/(r_sqrd^1.5)
+    ddu[2] = -GM*u[2]/(r_sqrd^1.5)
 end
 
 # initial conditions
-x0 = [1.0, 0.0] # units of AU
-dx0 = [0.0, 10.0] # units of AU/yr
-tspan = (0.0, 10.0)
+x0 = [r_peri, 0.f0] # units of AU
+dx0 = [0.f0, v_peri] # units of AU/yr
+tspan = (0.f0, 10.f0)
 
 # solve problem
-dx = 0.5
+dx = 0.4
 prob_newton = SecondOrderODEProblem(newton_gravity, dx0, x0, tspan)
-@time sol_newton = solve(prob_newton, saveat=dx)
+@time sol_newton = solve(prob_newton, Tsit5(), saveat=dx)
 sol_nts = [[sol_newton[i][3], sol_newton[i][4]] for i in eachindex(sol_newton)]
+
+scatter(getindex.(sol_nts,1),getindex.(sol_nts,2), label="", size=(400,400), dpi=200,
+        xlabel="\$x\$ (AU)", ylabel="\$y\$ (AU)")
+savefig("./plots/EPE_ODE_solution/newton_solution.png")
 
 @info "Solved ODE problem!"
 
@@ -91,19 +100,20 @@ function additional_loss(phi, θ, p)
     # 00 component of metric from neural network
     g00(x,y) = phi[1](sqrt(x^2+y^2), θ.depvar[:A])[1]
 
-    # set-up the problem using current 
+    # set-up the problem using current metric
     function simple_geodesic(ddu,du,u,p,t)
-        ddu[1] = -(c^2)/2 * ((g00(u[1]+ϵ,u[2]) - g00(u[1],u[2]))/ϵ)
-        ddu[2] = -(c^2)/2 * ((g00(u[1],u[2]+ϵ) - g00(u[1],u[2]))/ϵ)
+        # ddu[1] = -(c^2/2) * g00(u[1], u[2])
+        # ddu[2] = -(c^2/2) * g00(u[1], u[2])
+        ddu[1] = (c^2/2) * ((g00(u[1]+ϵ,u[2]) - g00(u[1]-ϵ,u[2]))/2ϵ)
+        ddu[2] = (c^2/2) * ((g00(u[1],u[2]+ϵ) - g00(u[1],u[2]-ϵ))/2ϵ)
     end
 
     # solve system of diff-eqs 
     prob = SecondOrderODEProblem(simple_geodesic, dx0, x0, tspan)
-    sol = solve(prob, Rosenbrock23(), reltol=0.01, abstol=0.01, saveat=dx)
+    sol = solve(prob, Rosenbrock32(), reltol=1e-1, abstol=1e-1, saveat=dx)
 
-    # return sum(distance2(sol_nts[i][1],sol[i][3],
-    #     sol_nts[i][2],sol[i][4]) for i in eachindex(sol_nts))
-    return sum(abs2, sol .- sol_newton)
+    return 0.1 * sum(distance2(sol_nts[i][1],sol[i][3],
+        sol_nts[i][2],sol[i][4]) for i in eachindex(sol_nts))
 end
 
 # -------------------------------------------------------------------------------------
@@ -113,17 +123,20 @@ dim = length(domains) # number of dimensions
 activation = Lux.σ
 nnodes = 10
 chains = [Lux.Chain(Lux.Dense(dim, nnodes, activation), 
+            Lux.Dense(nnodes, nnodes, activation),
             Lux.Dense(nnodes, 1)) for _ in 1:numChains]
 
-# run training on GPU if actailible 
+# run training on GPU if availible
 CUDA.allowscalar(false)
 ps = [Lux.setup(Random.default_rng(), chains[i])[1] for i in 1:numChains]
 ps = [ps[i] |> Lux.ComponentArray |> gpu .|> Float32 for i in 1:numChains]
 
 # discretize
 strategy = QuasiRandomTraining(20)
+#strategy = GridTraining(0.1)
+#strategy = QuadratureTraining()
 discretization = PhysicsInformedNN(chains, strategy,
-    additional_loss=additional_loss, init_params = ps)
+    additional_loss=additional_loss)
 @time prob = discretize(pde_sys, discretization)
 
 @info "Discretization complete. Beginning training"
@@ -133,17 +146,15 @@ i = 0
 loss_history = []
 
 # solve the problem!
-res = Optimization.solve(prob, ADAM(1e-4); callback = callback, maxiters=100)
+res = Optimization.solve(prob, ADAM(1e-3); callback = callback, maxiters=100)
 phi = discretization.phi
 
 @info "Training complete. Beginning analysis"
 
 # -------------------------------------------------------------------------------------
-using Plots, LaTeXStrings
-
 ## plot loss as a function of Epoch
 plot(1:length(loss_history), loss_history, xlabel="Epoch", ylabel="Loss",
-        size=(400,400), dpi=200, label="");
+        size=(400,400), dpi=200, label="")
 savefig("./plots/EPE_ODE_solution/loss.png")
 
 ## Compare solution to analytic!
@@ -159,15 +170,15 @@ u_real = [[u_analytic(r)[i] for r in rs] for i in 1:numChains]
 u_predict = [[phi[i]([r], minimizers[i])[1] for r in rs] for i in 1:numChains]
 
 plot(rs, u_real[1], xlabel=L"r", ylabel=L"A(r)", label="True Solution",
-        size=(400,400), dpi=200, legend=:bottomright);
+        size=(400,400), dpi=200)
 plot!(rs, u_predict[1], 
         label="Predicted Solution, \$\\chi^2/dof = $(round(χ²(u_predict[1], 
-            u_real[1])/length(u_predict[1]),digits=2))\$");
+            u_real[1])/length(u_predict[1]),digits=2))\$")
 savefig("./plots/EPE_ODE_solution/A.png")
 
 plot(rs, u_real[2], xlabel=L"r", ylabel=L"B(r)", label="True Solution",
-        size=(400,400), dpi=200, legend=:bottomright);
+        size=(400,400), dpi=200)
 plot!(rs, u_predict[2], 
         label="Predicted Solution, \$\\chi^2/dof = $(round(χ²(u_predict[2], 
-            u_real[2])/length(u_predict[2]),digits=2))\$");
+            u_real[2])/length(u_predict[2]),digits=2))\$")
 savefig("./plots/EPE_ODE_solution/B.png")
